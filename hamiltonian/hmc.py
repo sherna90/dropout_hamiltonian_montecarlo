930b5bad718271200c5fb5b03d72d858370d6cdf
Sergio <sherna90@yahoo.es>
2018-08-10 12:47:02 -0400

    hmc

--- hamiltonian/hmc.py
+++ hamiltonian/hmc.py
@@ -2,56 +2,28 @@
 import numpy as np
 import scipy as sp
 import os
-
+from collections import Iterable
 
 class Hamiltonian:
-    def __init__(self, logp, gradf, start, step_size=1, n_steps=5):
-
-        """ Hamiltonian MCMC sampler. Uses the gradient of log P(theta) to
-            make informed proposals.
-            Arguments
-            ----------
-            logp: function
-                log P(X) function for sampling distribution
-            start: dict
-                Dictionary of starting state for the sampler. Should have one
-                element for each argument of logp. So, if logp = f(x, y), then
-                start = {'x': x_start, 'y': y_start}
-            Keyword Arguments
-            -----------------
-            grad_logp: function or list of functions
-                Functions that calculate grad log P(theta). Pass functions
-                here if you don't want to use autograd for the gradients. If
-                logp has multiple parameters, grad_logp must be a list of
-                gradient functions w.r.t. each parameter in logp.
-            scale: dict
-                Same format as start. Scaling for initial momentum in
-                Hamiltonian step.
-            step_size: float
-                Step size for the deterministic proposals.
-            n_steps: int 
-                Number of deterministic steps to take for each proposal.
-            """
+    def __init__(self, logp, grad, start, step_size=1, n_steps=5):
         self.start = start
-        self.step_size = step_size / (len(self.start)**(1/4)
+        self.step_size = step_size/(len(self.start))**(1/4)
         self.n_steps = n_steps
         self.logp = logp
-        self.state = {}
+        self.grad=grad
+        self.state = start
 
 
     def step(self):
-        x = self.state
-        r0 = initial_momentum()
-        y, r = x, r0
-
+        q = self.state
+        p = self.draw_momentum()
+        y, r = q, p
         for i in range(self.n_steps):
-            y, r = leapfrog(y, r, self.step_size, self.model.grad)
-
-        if accept(x, y, r0, r, self.model.logp):
-            x = y
+            y, r = leapfrog(y, r, self.step_size, self.grad)
+        if accept(q, y, p, r, self.logp):
+            q = y
             self._accepted += 1
-
-        self.state = x
+        self.state = q
         self._sampled += 1
         return x
 
@@ -59,30 +31,45 @@ class Hamiltonian:
     def acceptance_rate(self):
         return self._accepted/self._sampled
 
+    def flatten(items):
+        for x in items:
+            if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+                for sub_x in flatten(x):
+                    yield sub_x
+            else:
+                yield x
 
-    def leapfrog(self,x, r, step_size, grad):
-        r1 = r + step_size/2*grad(x)
-        x1 = x + step_size*r1
-        r2 = r1 + step_size/2*grad(x1)
-        return x1, r2
+    def leapfrog(self,q, p, step_size, grad):
+        p = p + step_size/2*grad(x)
+        q = q + step_size*p
+        p = p + step_size/2*grad(x1)
+        return q, p
 
 
-    def accept(self,x, y, r_0, r, logp):
-        E_new = energy(logp, y, r)
-        E = energy(logp, x, r_0)
+    def accept(self,q, y, p, r):
+        E_new = energy(y, r)
+        E = energy(q, p)
         A = np.min(np.array([0, E_new - E]))
         return (np.log(np.random.rand()) < A)
 
 
-    def energy(self,logp, x, r):
-        return logp(x) - 0.5*np.dot(r1, r1)
+    def energy(self,logp, q, p):
+        pf=list(flatten(p.values()))
+        return self.logp(q) - 0.5*np.dot(pf, pf)
 
 
-    def initial_momentum(self):
-        new = {}
+    def draw_momentum(self):
+        momentum = {}
         for var in self.start:
-            new[var]=np.random.randn(len(self.start[var]))
-        return new
+            dim=(np.array(self.start[var])).size
+            if dim==1:
+                momentum[var]=np.random.normal(0,1)
+            else:
+                mass_matrix=np.identity(dim)
+                momentum[var]=np.random.multivariate_normal(np.zeros(dim), mass_matrix)
+        return momentum
+
 
     def sample(niter=1e3):
         for i in range(niter):
+            self.step()
