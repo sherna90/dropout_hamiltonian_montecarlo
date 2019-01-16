21ef6555e8241f732cb5847c16f953ff125cd998
Sergio <sherna90@yahoo.es>
2019-01-16 11:37:34 -0300

    MC dropout

--- benchmarks/plants/test_plants_sgd.py
+++ benchmarks/plants/test_plants_sgd.py
@@ -91,13 +91,11 @@ def plot_confusion_matrix(cm, classes,
     plt.yticks(tick_marks, classes)
     fmt = '.1f' if normalize else 'd'
     thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
+
+np.savez("sgd_plants.npz",par_sgd=par_sgd,par_sgd_dropout_05=par_sgd_dropout_05,par_sgd_dropout_01=par_sgd_dropout_01,par_sgd_dropout_09=par_sgd_dropout_09)
 
 plt.figure()
 plot_confusion_matrix(cnf_matrix_sgd, classes=np.int32(classes),title='SGD')
